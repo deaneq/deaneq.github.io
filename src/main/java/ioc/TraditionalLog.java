b94b1f44532829050800a40e03ce1070d928f27d
ana.escudero@gmail.com <ana.escudero@gmail.com>
2018-11-30 13:37:05 +0100

    Conflicte A

--- src/main/java/ioc/TraditionalLog.java
+++ src/main/java/ioc/TraditionalLog.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * @author Xavi Torrens
  */
-//Segon canvi, quest en una nova branca
+//Segon canvi, quest en una (conflicte usuari A) nova branca
 //Canvi fet i pujat per B
 // I ara treballant sobre versió que ha pujat A i jo no he actualitzat
 public class TraditionalLog {
